a06bbd49d6e790c68fd871bd3ff052456244dce5
swc_20200029 <43691125+jirufengyu@users.noreply.github.com>
2022-01-08 09:40:50 +0800

    adsf

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("M:\\MineverseWeb-master\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("M:\\MineverseWeb-master\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("M:\\MineverseWeb-master\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
+  require("M:\\MineverseWeb-master\\src\\css\\custom.css"),
+];
